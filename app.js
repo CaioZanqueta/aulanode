c4005e15e5796b77184e56c784831a4634c27bff
Caio Augusto Conceiçao Zanqueta <caio.zanqueta@compasso.com.br>
2022-01-27 14:42:33 -0300

    Aula node

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const url = 'mongodb+srv://usuario_admin:usuario_admin@clusterapi.31wkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(url);
+
+mongoose.connection.on('error', (err) => {
+  console.log('Erro na conexão com o banco de dados: ' + err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Aplicação desconectada do bando de dados!');
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Aplicação conectada ao bando de dados!');
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const indexRoute = require('./Routes/index');
+const usersRoute = require('./Routes/users');
+
+app.use('/', indexRoute);
+app.use('/users', usersRoute);
+
+app.listen(3000);
+
+module.exports = app;
